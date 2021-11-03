557ff6d339f107e7a61823134cfaeb3c889baeb0
Paul Seal <prjseal@gmail.com>
2021-11-03 23:07:21 +0000

    2nd episode first commit

---
+++ MyStarterKit/Migrations/StarterKitPackageMigrationPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Umbraco.Cms.Core.Packaging;
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace MyStarterKit.Migrations
+{
+    public class StarterKitPackageMigrationPlan : PackageMigrationPlan
+    {
+        public StarterKitPackageMigrationPlan()
+            : base("MyStarterKit")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<ImportPackageXmlMigration>(new Guid("CDFB34A5-0EF9-4E62-AC7A-C6E2C0CE4AF7"));
+        }
+    }
+}
